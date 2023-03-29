290be407fbf48a93d93b0cf0edc9f3c49a7a0015
LinaTitova <126906386+LinaTitova@users.noreply.github.com>
2023-03-29 15:36:58 +0300

    Max_program

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,3 +1,9 @@
-﻿int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine(a*a);
+﻿String s_a = Console.ReadLine()!;
+String s_b = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+if (a<b) Console.WriteLine("Max b="+b);
+else Console.WriteLine("Max:"+a);
+
+
 
