1e01b764ad20874a27f7788e6cfa64250846b65f
LinaTitova <126906386+LinaTitova@users.noreply.github.com>
2023-03-29 16:11:00 +0300

    ex_2

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿String s_a = Console.ReadLine()!;
+String s_b = Console.ReadLine()!;
+String s_c = Console.ReadLine()!;
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+if (a<b&&b>c) Console.WriteLine("Max:"+b);
+else if (a<c&&b<c) Console.WriteLine("Max:"+c);
+else Console.WriteLine("Max:"+a);
